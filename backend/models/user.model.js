9c677a43dabd9ea7d51dcaea850261f516e9cc2d
Tarun <tarunkumar23j@gmail.com>
2025-02-08 12:28:11 +0530

    userModel created having schema and some methods like generate token, hash password and compare password inside user.model.js file and require it in user.controller.js file

---
+++ backend/models/user.model.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const bcrypt = require('bcrypt');
+
+const jwt = require('jsonwebtoken');
+
+const userSchema = new mongoose.Schema({
+    fullname:{
+        firstname:{
+            type:String,
+            required:true,
+            minlength: [3, "First name must be atleast 3 characters long"]
+        },
+        lastname:{
+            type:String,
+            minlength: [3, "Last name must be atleast 3 characters long"]
+        },
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        minlength: [5, "Email must be atleast 5 characters long"]
+    },
+    password:{
+        type:String,
+        required:true,
+        // Whenever I find user I dont want to send password of it to prevent this select : false
+        select:false
+    },
+    // for live tracking so user can track live location of driver or captain.
+    socketId:{
+        type:String
+    }
+})
+
+userSchema.methods.generateAuthToken = function() {
+    const token = jwt.sign({ _id: this._id }, process.env.SECRET_KEY);
+    return token;
+}
+
+userSchema.statics.hashPassword = async function (password) {
+    return await bcrypt.hashPassword(password, 10);
+}
+
+userSchema.methods.comparePassword = async function (passowrd){
+    return await bcrypt.compare(passowrd, this.password);
+}
